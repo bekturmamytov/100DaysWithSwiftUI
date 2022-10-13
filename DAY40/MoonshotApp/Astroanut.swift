72fd9fdaad751ea2c92f2421de2f190b78867dab
bekturmamytov <bekturmamytov@gmail.com>
2022-10-14 05:13:22 +0900

    has been completed all works except design. Now i have started to create a design of UI.

---
+++ DAY40/MoonshotApp/Astroanut.swift
@@ -0,0 +1,14 @@
+//
+//  Astroanut.swift
+//  MoonshotApp
+//
+//  Created by Bektur Mamytov on 14/10/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ DAY40/MoonshotApp/Bundle-Decodable.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle-Decodable.swift
+//  MoonshotApp
+//
+//  Created by Bektur Mamytov on 14/10/22.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        //find the file from bundle
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        //loading file from bundle
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        //decoding the file from bundle
+        let decoder  = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
